ee5ccd58639e3ae122d2da1a0961365004d8c3f9
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-28 17:07:09 -0500

    Pantalla de detalle de la una pregunta 3

--- src/app/question/question-detail.component.ts
+++ src/app/question/question-detail.component.ts
@@ -3,7 +3,8 @@ import { Question } from './question.model';
 
 @Component({
   selector: 'app-question-detail',
-  templateUrl: './question-detail.component.html'
+  templateUrl: './question-detail.component.html',
+  styleUrls: ['./question-detail.component.css']
 })
 export class QuestionDetailComponent {
   question: Question = new Question(
