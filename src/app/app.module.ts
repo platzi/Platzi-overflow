086b4112c22fb5a59f52cba771b0156313fb0c11
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-28 17:12:57 -0500

    Incorporando moment a nuestro proyecto para mostrar fechas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import 'hammerjs';
 
 import { QuestionDetailComponent } from './question/question-detail.component';
 
+import { MomentModule } from 'angular2-moment';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +20,8 @@ import { QuestionDetailComponent } from './question/question-detail.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    MomentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
