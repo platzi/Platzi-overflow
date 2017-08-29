a530adab8bb97c17bd05d67f9a661ce1ce174d64
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-29 10:52:56 -0500

    Dando estilo al formulario de respuestas

--- src/app/answer/answer-form.component.ts
+++ src/app/answer/answer-form.component.ts
@@ -3,7 +3,16 @@ import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-answer-form',
-  templateUrl: './answer-form.component.html'
+  templateUrl: './answer-form.component.html',
+  styles: [`
+    form {
+      display: flex;
+      flex-direction: column;
+      width: 90%;
+      margin: 40px auto;
+      margin-top: 20px;
+    }
+  `]
 })
 export class AnswerFormComponent {
   onSubmit(form: NgForm) {
