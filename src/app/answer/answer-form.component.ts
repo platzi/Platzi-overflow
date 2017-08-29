e0d6a44813a77fa327b9fd1a63f8a564303e66f5
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-29 11:07:40 -0500

    Agregando la respuesta al listado

--- src/app/answer/answer-form.component.ts
+++ src/app/answer/answer-form.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Answer, User } from './answer.model';
+import { Question } from '../question/question.model';
 
 @Component({
   selector: 'app-answer-form',
@@ -15,7 +17,16 @@ import { NgForm } from '@angular/forms';
   `]
 })
 export class AnswerFormComponent {
+  @Input() question: Question;
+
   onSubmit(form: NgForm) {
-    console.log(form.value.description);
+    const answer = new Answer(
+      form.value.description,
+      this.question,
+      new Date(),
+      new User('Paula', 'Becerra')
+    );
+    this.question.answers.unshift(answer);
+    form.reset();
   }
 }
