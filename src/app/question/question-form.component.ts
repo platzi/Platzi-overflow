6489e2400bc904b742fb2fd6ff8835d980931cc0
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-29 15:32:47 -0500

    Crear formulario de carga de nueva pregunta

--- src/app/question/question-form.component.ts
+++ src/app/question/question-form.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Question } from './question.model';
 
 @Component({
   selector: 'app-question-form',
   templateUrl: './question-form.component.html'
 })
 export class QuestionFormComponent {
-
+  onSubmit(form: NgForm) {
+    const q = new Question(
+      form.value.title,
+      form.value.description
+    );
+    console.log(q);
+  }
 }
