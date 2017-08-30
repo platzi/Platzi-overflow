b81c54a45949fdd57558cec3ddc7ce32d428eb74
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-30 16:01:11 -0500

    Agregando una ruta para crear una pregunta en el backend

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import { question } from './routes'
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 if (process.env.NODE_ENV === 'development') {
   app.use((req, res, next) => {
--- server/routes/question.js
+++ server/routes/question.js
@@ -19,10 +19,30 @@ const question = {
 
 const questions = new Array(10).fill(question)
 
-// /api/questions
+// GET /api/questions
 app.get('/', (req, res) => res.status(200).json(questions))
 
-// /api/questions/:id
-app.get('/:id', (req, res) => res.status(200).json(question))
+// GET /api/questions/:id
+app.get('/:id', (req, res) => {
+  const { id } = req.params
+  const q = questions.find(({ _id }) => _id === +id)
+  res.status(200).json(q)
+})
+
+// POST /api/questions
+app.post('/', (req, res) => {
+  const question = req.body
+  question._id = +new Date()
+  question.user = {
+    email: 'sacha@platzi.com',
+    password: '123456',
+    firstName: 'Sacha',
+    lastName: 'Lifszyc'
+  }
+  question.createdAt = new Date()
+  question.answers = []
+  questions.push(question)
+  res.status(201).json(question)
+})
 
 export default app
