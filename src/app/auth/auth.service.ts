ecd4cd6544844dbca20f6c796b1d9c93f30d66b4
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-30 18:15:31 -0500

    Escribiendo nuestro servicio de autenticación

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import urljoin from 'url-join';
+import { environment } from '../../environments/environment';
+import { User } from './user.model';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class AuthService {
+  usersUrl: string;
+  currentUser?: User;
+
+  constructor(private http: Http) {
+    this.usersUrl = urljoin(environment.apiUrl, 'auth');
+    if (this.isLoggedIn()) {
+      const { userId, email, firstName, lastName } = JSON.parse(localStorage.getItem('user'));
+      this.currentUser = new User(email, null, firstName, lastName, userId);
+    }
+  }
+
+  signin(user: User) {
+    const body = JSON.stringify(user);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    return this.http.post(urljoin(this.usersUrl, 'signin'), body, { headers })
+      .map((response: Response) => {
+        const json = response.json();
+        this.login(json);
+        return json;
+      })
+      .catch((error: Response) => {
+        console.log(error);
+        return Observable.throw(error.json());
+      });
+  }
+
+  login = ({ token, userId, firstName, lastName, email }) => {
+    this.currentUser = new User(email, null, firstName, lastName, userId);
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify({ userId, firstName, lastName, email }));
+  }
+
+  isLoggedIn() {
+    return localStorage.getItem('token') !== null;
+  }
+}
--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -3,7 +3,8 @@ export class User {
     public email: string,
     public password: string,
     public firstName?: string,
-    public lastName?: string
+    public lastName?: string,
+    public _id?: string
   ) { }
 
   fullName() {
