97b3078e24aae52216ae26609091d76e5ffbc1ae
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-30 16:13:10 -0500

    Agregando el método para crear preguntas en el QuestionService

--- src/app/question/question.service.ts
+++ src/app/question/question.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Question } from './question.model';
-import { Http } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
 import { environment } from '../../environments/environment';
 import urljoin from 'url-join';
 import 'rxjs/add/operator/toPromise';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Injectable()
 export class QuestionService {
@@ -27,6 +29,15 @@ export class QuestionService {
             .toPromise()
             .then(response => response.json() as Question)
             .catch(this.handleError);
+  }
+
+  addQuestion(question: Question) {
+    const body = JSON.stringify(question);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+
+    return this.http.post(this.questionsUrl, body, { headers })
+      .map((response: Response) => response.json())
+      .catch((error: Response) => Observable.throw(error.json()));
   }
 
   handleError(error: any) {
