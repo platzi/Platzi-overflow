4ebe07a5bd099ae0da217c250a21f868f470f38f
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-31 10:42:25 -0500

    Creando la ruta de signup en el backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -34,10 +34,10 @@ app.post('/signin', (req, res, next) => {
 
   if (!comparePasswords(password, user.password)) {
     debug(`Passwords do not match: ${password} !== ${user.password}`)
-    return handleLoginFailed(res)
+    return handleLoginFailed(res, 'El correo y la contraseña no coinciden')
   }
 
-  const token = jwt.sign({ user }, secret, { expiresIn: 86400 })
+  const token = createToken(user)
   res.status(200).json({
     message: 'Login succeded',
     token,
@@ -48,10 +48,34 @@ app.post('/signin', (req, res, next) => {
   })
 })
 
-function handleLoginFailed(res) {
+const createToken = (user) => jwt.sign({ user }, secret, { expiresIn: 86400 })
+
+app.post('/signup', (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+  const user = {
+    _id: +new Date(),
+    firstName,
+    lastName,
+    email,
+    password
+  }
+  debug(`Creating new user: ${user}`)
+  users.push(user)
+  const token = createToken(user)
+  res.status(201).json({
+    message: 'User saved',
+    token,
+    userId: user._id,
+    firstName,
+    lastName,
+    email
+  })
+})
+
+function handleLoginFailed(res, message) {
   return res.status(401).json({
     message: 'Login failed',
-    error: 'Email and password don\'t match'
+    error: message || 'Email and password don\'t match'
   })
 }
 
