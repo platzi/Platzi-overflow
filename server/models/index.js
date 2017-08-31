44d417557825432fcc5a6c08f1c9b930dc8e736c
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2017-08-31 15:22:03 -0500

    Creando el modelo de pregunta

---
+++ server/models/index.js
@@ -0,0 +1,3 @@
+export { default as User } from './user'
+export { default as Answer } from './answer'
+export { default as Question } from './question'
---
+++ server/models/question.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from 'mongoose'
+
+const { ObjectId } = Schema.Types
+
+const QuestionSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  icon: { type: String, required: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  user: { type: ObjectId, ref: 'User', required: true }
+  answers: [{ type: ObjectId, ref: 'Answer' }]
+})
+
+export default mongoose.model('Question', QuestionSchema)
